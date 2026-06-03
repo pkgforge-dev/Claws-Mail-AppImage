e949a0a5ea4c50b9d5f09a11405d12716ba7cd9e
Samueru-sama <xdglawyer@outlook.com>
2026-06-03 19:09:50 -0400

    package app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,16 +6,23 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+	bogofilter   \
+	claws-mail   \
+	ghostscript  \
+	gumbo-parser \
+	libarchive   \
+	libcanberra  \
+	libical      \
+	libnotify    \
+	perl         \
+	poppler-glib \
+	spamassassin \
+	webkit2gtk-4.1
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
-
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,22 +3,24 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q claws-mail | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/128x128/apps/claws-mail.png
+export DESKTOP=/usr/share/applications/claws-mail.desktop
 
-# Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
-
-# Additional changes can be done in between here
+# Deploy the application, its plugins, web extensions and all their dependencies
+quick-sharun \
+	/usr/bin/claws-mail \
+	/usr/lib/claws-mail \
+	/usr/bin/perl       \
+	/usr/lib/perl5      \
+	/usr/lib/libwebkit2gtk-*.so*
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+# Test the AppImage (GUI apps may need --simple-test to avoid xvfb timeout)
+quick-sharun --simple-test ./dist/*.AppImage
