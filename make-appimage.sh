d4dc8d6efd072279eefe289b8607b735153f149d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-06-03 20:46:28 -0400

    lower strace time

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/claws-mail.png
 export DESKTOP=/usr/share/applications/claws-mail.desktop
+export STRACE_TIME=2
 
 # Deploy the application, its plugins, web extensions and all their dependencies
 quick-sharun \
